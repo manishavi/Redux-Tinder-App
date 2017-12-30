5a0c294204db8a0d47d74cdbf7b21fe32331542a
manishavi <manishalal972@gmail.com>
2017-12-29 19:37:16 -0600

    added containers and reducers

---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'React Learning', pages: 101 },
+    { title: 'Redux Processing', pages: 39 },
+    { title: 'Node Introduction', pages: 85 },
+    { title: 'Monogo Connect' , pages: 1 },
+  ];
+    
+}
